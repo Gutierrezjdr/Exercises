ee29bd716dace3f5bd29e7ec0c7fe20236540355
Jessica Gutierrez <gutierrezjdr@gmail.com>
2018-02-07 12:22:57 -0500

    Finally Deleted Garbage After Array
    Added code after the lopped that made sure to stop garbage from being output after the char array. FINALLY

--- parsing.cpp
+++ parsing.cpp
@@ -1,46 +1,45 @@
 #include<iostream>
-#include<string>
+
 using namespace std;
 
 int main(){
-  char data[21];
-  char yr[4],mon[2],day[2],hour[2],min[2],sec[2];
+  char data[21]={'\0'};
+  char yr[4]={'\0'};
+  char mon[2]={'\0'};
+  char day[2]={'\0'};
+  char hour[2]={'\0'};
+  char min[2]={'\0'};
+  char sec[2]= {'\0'};
   int loc=0;
+   
   cout<<"Input as YYYYMMDDTHHMMSS: ";
   cin>>data;
-  cout<<data; 
-  for(int i=0;i<18;i++){
-    if(i<4){
-      yr[loc]=data[i];
-      loc++;
-     }
-    if(i==4) loc=0;
-    if(i>3&&i<6){
-      mon[loc]=data[i];
-      loc++;
-     }
-    if(i==6) loc=0;
-    if(i>5&&i<8){
-     day[loc]=data[i];
-     loc++;
-    } 
-    if(i==8) loc=0;
-    if(i>8&&i<11){
-     hour[loc]=data[i];
-     loc++;
-    }
-   if(i==11)loc=0;
-   if(i>10&&i<13){
-    min[loc]=data[loc];
-    loc++;
-    }
+  cout<<data;
+  int i; 
+  for( i=0;i<18;i++){
+    cout<<"\nI: "<<i<<": "<<data[i]<<" ";
+    if(i==4||i==6||i==9||i==11||i==13) loc=0;
+    if(i<4)  yr[loc]=data[i];
+    if(i>=0&&i<3)  loc++;
+    if(i>3&&i<6) mon[loc]=data[i];
+    if(i>5&&i<8) day[loc]=data[i]; 
+    if(i>8&&i<11) hour[loc]=data[i];
+   if(i>10&&i<13)  min[loc]=data[i];
+   if(i>12&&i<15)  sec[loc]=data[i];
+ 
+    if(i==4||i==6||i==9||i==11||i==13) loc++;
   }
+  yr[4]='\0';
+  mon[2]='\0';
+  day[2]='\0';
+  hour[2]='\0';
+  min[2]='\0'; 
+  sec[2]='\0';
   cout<<"\nyr: "<<yr<<endl;
   cout<<"mon: "<<mon<<endl;
   cout<<"day: "<<day<<endl;
   cout<<"hour: "<<hour<<endl;
   cout<<"min: "<<min<<endl;
   cout<<"sec: "<<sec<<endl;
-  cout<<data.size();
-
+  return 0;
 }
